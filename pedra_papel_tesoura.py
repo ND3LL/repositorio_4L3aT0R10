6f4e73c208d1ddc170965cde0a52551033d41c33
nd3ll <nd3ll@protonmail.com>
2024-08-28 10:46:51 -0300

    Deletei o alterrar_barra.py e finalizei o pedra_papel_tesoura.py

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -3,7 +3,7 @@ from random import choice
 def exibir_placar(placar_player, placar_computador, nick):
 	print('\n')
 	print('Placar')
-	print(f'{nick}: {placar_player')
+	print(f'{nick}: {placar_player}')
 	print(f'Computador: {placar_computador}')
 	print('\n')
 	
@@ -22,13 +22,24 @@ while rodadas <= 5:
 		rodadas += 1
 		placar_player += 1
 		print(f'{nick} ganhou a rodada!')
+		print('Palpites:')
+		print(f'\n{nick} > {palpite}')
+		print(f'Computador > {palpite_computador}')
 		exibir_placar(placar_player, placar_computador, nick)
 		
 	if palpite == 'pedra' and palpite_computador == 'papel' or palpite == 'papel' and palpite_computador == 'tesoura' or palpite == 'tesoura' and palpite_computador == 'pedra':
 		print(f'{nick} perdeu a partida :/')
+		print(f'\n{nick} > {palpite}')
+		print(f'Computador > {palpite_computador}')
 		placar_computador +=1
 		rodadas += 1
 		exibir_placar(placar_player, placar_computador, nick)
 		
 	if palpite == palpite_computador:
 		print('Empate!')
+
+if placar_player > placar_computador:
+	print(f'{nick} ganhou o jogo!!!')
+	exibir_placar(placar_player, placar_computador, nick)
+else:
+	print('Computador ganhou o jogo!!!')
